b27327b26e0565ad4d0329b7aa7a187fd2f36f31
isaac <baa44422@gmail.com>
2021-08-10 15:05:35 +0800

    Implemented persistence

--- Weirder Reminder/ReminderData.swift
+++ Weirder Reminder/ReminderData.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ReminderData: ObservableObject {
+    @Published var reminders: [Reminder] = []
+    
+    let sampleReminders = [Reminder(name: "TEsting", isCompleted: false)]
+    
+    func getArchiveURL() -> URL {
+        let plistName = "Reminders.plist"
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        return documentsDirectory.appendingPathComponent(plistName)
+    }
+    
+    func save() {
+        let archiveURL = getArchiveURL()
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedReminders = try? propertyListEncoder.encode(reminders)
+        try? encodedReminders?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    func load() {
+        let archiveURL = getArchiveURL()
+        let propertyListDecoder = PropertyListDecoder()
+        
+        var finalReminders: [Reminder]!
+        
+        if let retrievedReminderData = try? Data(contentsOf: archiveURL),
+            let decodedReminders = try? propertyListDecoder.decode([Reminder].self, from: retrievedReminderData) {
+            finalReminders = decodedReminders
+        } else {
+            finalReminders = sampleReminders
+        }
+        
+        reminders = finalReminders
+    }
+}
