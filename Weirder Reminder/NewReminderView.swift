ba2c53c1dd4b4d88bba7a2b83c65179008b88529
isaac <baa44422@gmail.com>
2021-08-10 14:52:38 +0800

    Made a form so that the users can create new reminders

--- Weirder Reminder/NewReminderView.swift
+++ Weirder Reminder/NewReminderView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct NewReminderView: View {
+    
+//    Binding vars here
+    @Binding var reminders: [Reminder]
+    
+//    State vars here
+    @State var name = ""
+    
+//    Environment Values here
+    @Environment(\.presentationMode) var scenePhase
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        Start of body
+        Form {
+//            Start of Form
+            Section(header: Text("Name")) {
+//                Start of Section
+                TextField("Name", text: $name)
+            }
+//            End of Section
+            Section {
+//                Start of Section
+                Button {
+//                    Start of Button
+//                    Start of Action
+                    reminders.append(Reminder(name: name, isCompleted: false))
+                    scenePhase.wrappedValue.dismiss()
+//                    End of Action
+                } label: {
+//                    Start of Label
+                    Text("Save")
+//                    End of Label
+//                    End of Button
+                }
+                
+                Button {
+//                    Start of Button
+//                    Start of Action
+                    
+                    scenePhase.wrappedValue.dismiss()
+//                    End of Action
+                } label: {
+//                    Start of Label
+                    Text("Dismiss")
+                        .foregroundColor(.red)
+//                    End of Label
+//                    End of Button
+                }
+//                End of Section
+            }
+//            End of Form
+        }
+//        End of Body
     }
 }
 
 struct NewReminderView_Previews: PreviewProvider {
     static var previews: some View {
-        NewReminderView()
+        NewReminderView(reminders: .constant([Reminder(name: "DA", isCompleted: false)]))
     }
 }
