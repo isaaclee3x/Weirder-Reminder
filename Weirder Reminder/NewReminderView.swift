43eba4a94b597baabf96655e154b858a0d35835b
isaac <baa44422@gmail.com>
2021-08-10 14:10:07 +0800

    Created a new view for creating new reminders

---
+++ Weirder Reminder/NewReminderView.swift
@@ -0,0 +1,20 @@
+//
+//  NewReminderView.swift
+//  Weirder Reminder
+//
+//  Created by Isaac Lee Jing Zhi on 10/8/21.
+//
+
+import SwiftUI
+
+struct NewReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewReminderView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewReminderView()
+    }
+}
