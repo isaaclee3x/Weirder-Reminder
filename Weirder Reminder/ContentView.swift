7ab2517f9b2d421dfa65bd1db2cad352bd8772e6
isaac <baa44422@gmail.com>
2021-08-10 13:29:21 +0800

    Implementing delete and move, and also part 1 of toggle

--- Weirder Reminder/ContentView.swift
+++ Weirder Reminder/ContentView.swift
@@ -21,11 +21,38 @@ struct ContentView: View {
 //                Start of ForEach
 //                This is so that we can implement the onDelete and onMove functions
                 ForEach(reminders) { reminder in
-                    
+                    Button {
+//                        Start of Button
+                        let reminderIndex = reminders.firstIndex(of: reminder)!
+                        reminders[reminderIndex].isCompleted.toggle()
+                        
+                    } label: {
+                        Text(reminder.name)
+                        
+                    }
+//                    End of Button
                 }
+                .onDelete { indexSet in
+//                    Start of onDelete
+                    reminders.remove(atOffsets: indexSet)
+//                    End of onDelete
+                }
+                .onMove { source, destination in
+//                    Start of onMove
+                    reminders.move(fromOffsets: source, toOffset: destination)
+//                    End of onMove
+                }
+                
+//                End of ForEach
             }
+            .navigationTitle("Reminders")
+            .navigationBarItems(leading: EditButton())
+//            End of List
         }
+//        End of NavigationView
     }
+//    End of Body
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
