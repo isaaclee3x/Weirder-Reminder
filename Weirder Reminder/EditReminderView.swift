c55c5f8d1dc0411f9ab563418b855ef3bc4daceb
isaac <baa44422@gmail.com>
2021-08-13 21:46:47 +0800

    START FROM V1.1

--- Weirder Reminder/EditReminderView.swift
+++ Weirder Reminder/EditReminderView.swift
@@ -2,19 +2,55 @@
 //  EditReminderView.swift
 //  Weirder Reminder
 //
-//  Created by Isaac Lee Jing Zhi on 13/8/21.
+//  Created by Isaac Lee Jing Zhi on 10/8/21.
 //
 
 import SwiftUI
 
 struct EditReminderView: View {
+    
+    //    Binding vars here
+    @Binding var reminders: [Reminder]
+    @Binding var index: Int
+    
+    //    State vars here
+    @State var name = ""
+    
+//    Environment vars here
+    @Environment (\.presentationMode) var scenePhase
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("NAME")) {
+                    TextField(reminders[index].name, text: $name)
+                }
+                
+                Section {
+                    Button {
+                        reminders[index].name = name
+                        scenePhase.wrappedValue.dismiss()
+                    } label: {
+                        Text("Edit")
+                        
+                    }
+                    Button {
+                        scenePhase.wrappedValue.dismiss()
+                    } label: {
+                        Text("Dismiss")
+                            .foregroundColor(.red)
+                        
+                    }
+                }
+            }
+            .navigationTitle("Edit Reminder")
+        }
     }
 }
 
+
 struct EditReminderView_Previews: PreviewProvider {
     static var previews: some View {
-        EditReminderView()
+        EditReminderView(reminders: .constant([Reminder(name: "ACA", isCompleted: false, tagColor: RGB(r: "0", g: "1", b: "0"), tagString: "ASa")]), index: .constant(0))
     }
 }
